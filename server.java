4123450286ed28e9dfb8e0681f123d1356ff1a84
oscarcederberg <os7138ce-s@student.lu.se>
2021-03-01 16:49:23 +0100

    Arrays -> hashmaps to store indivs. Simple create-implementation.

--- server.java
+++ server.java
@@ -9,23 +9,42 @@ import javax.security.cert.X509Certificate;
 public class server implements Runnable {
     private ServerSocket serverSocket = null;
     private static int numConnectedClients = 0;
+    private HashMap<String, Doctor> doctorMap;
+    private HashMap<String, Nurse> nurseMap;
+    private HashMap<String, Gov> govMap;
+    private HashMap<String, Patient> patientMap;
     private HashMap<String, Record> db;
     
     public server(ServerSocket ss) throws IOException {
         serverSocket = ss;
         newListener();
-	Indiv[] doctors = new Indiv[100];
-	doctors[0] = new Doctor("Elna", "Data");
+
+        /*
+        Indiv[] doctors = new Indiv[100];
+        doctors[0] = new Doctor("Elna", "Data");
         Indiv[] nurses = new Indiv[100];
-	nurses[0] = new Nurse("Gabriel", "Data");
-	Indiv[] govs = new Indiv[100];
-	govs[0] = new Gov("Patrik");
-	Record r1 = new Record( (Doctor)doctors[0], (Nurse)nurses[0], "Data", "This patient has a severe headache! Prescribing Paracetamol in large doses.");
-	Indiv[] patients = new Indiv[100];
-	patients[0] = new Patient("Oscar", r1);
-	
-	this.db = new HashMap();
-	db.put(patients[0].toString(), r1);
+        nurses[0] = new Nurse("Gabriel", "Data");
+        Indiv[] govs = new Indiv[100];
+        govs[0] = new Gov("Patrik");
+        Record r1 = new Record( (Doctor)doctors[0], (Nurse)nurses[0], "Data", "This patient has a severe headache! Prescribing Paracetamol in large doses.");
+        Indiv[] patients = new Indiv[100];
+        patients[0] = new Patient("Oscar", r1);
+        */
+        
+        this.doctorMap = new HashMap();
+        doctorMap.put("Elna", new Doctor("Elna", "Data"));
+        this.nurseMap = new HashMap();
+        nurseMap.put("Gabriel", new Nurse("Gabriel", "Data"));
+        this.govMap = new HashMap();
+        govMap.put("Patrik", new Gov("Patrik"));
+
+        Record r1 = new Record( doctorMap.get("Elna"), nurseMap.get("Gabriel"), "Data", "This patient has a severe headache! Prescribing Paracetamol in large doses.");
+
+        this.patientMap = new HashMap();
+        patientMap.put("Oscar", new Patient("Oscar", r1));
+        
+        this.db = new HashMap();
+	    db.put("Oscar", r1);
     }
 
     public void run() {
@@ -48,48 +67,50 @@ public class server implements Runnable {
 
             String clientMsg = null;
             while ((clientMsg = in.readLine()) != null) {
-	    //If the user is authentic, then the clientmessage will be a correct 
-		String res = "";
-		String[] input = clientMsg.split(" ");
-		String command = "";
-		String argument1 = "";
-		String argument2 = "";
-		
-		try{
-			command = input[0];
-			System.out.println("Before the split");
-			argument1 = input[1];
-			argument2 = input[2];
-			
-			System.out.println("After the split");
-		} catch(Exception e){
-			
-		}		
+            //If the user is authentic, then the clientmessage will be a correct 
+            String res = "";
+            String[] input = clientMsg.split(" ");
+            String command = input[0];
 
-		switch(command){
-			case "read": {
-    				res = db.get(argument1).toString();
- 			}
-			case "write": {
-				res = db.get(argument1).write(argument1, argument2);
-			}
-			case "create": {
-				res = "tried to create some data";
-			}
-			case "delete": {
-				res = "tried to delete some data";
-			}
-			default: {
-				res = "Command not found";
-			}
+            switch(command){
+                case "read": {
+                    if(input.length == 2){
+                        res = db.get(input[1]).toString();
+                    }
                 }
+                case "write": {
+                    if(input.length == 3){
+                        res = db.get(input[1]).write(input[1], input[2]);
+                    }
+                }
+                case "create": {
+                    if(input.length == 4){
+                        Patient patient = patientMap.get(input[1]);
+                        Doctor doctor = doctorMap.get(input[2]);
+                        Nurse nurse = nurseMap.get(input[3]);
+                        String department = input[4];
+                        String data = input[5];
+                        if(patient != null && doctor != null && nurse != null){
+                            Record record = new Record(doctor,nurse,department,data);
+                            db.put(patient.toString(), record);
+                        }
+                    }
+                    res = "tried to create some data";
+                }
+                case "delete": {
+                    res = "tried to delete some data";
+                }
+                default: {
+                    res = "Command not found";
+                }
+            }
 		
-		System.out.println("received '" + clientMsg + "' from " + subject);
-                System.out.print("sending information to client...");
-				out.println(res);
-				out.flush();
-                System.out.println("done\n");
-			}
+		    System.out.println("received '" + clientMsg + "' from " + subject);
+            System.out.print("sending information to client...");
+			out.println(res);
+			out.flush();
+            System.out.println("done\n");
+		    }
 			in.close();
 			out.close();
 			socket.close();
